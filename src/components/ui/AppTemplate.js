39d420696e3624dd35ff5a38837e7c702b6c69fa
Chris-Fortier <vendor@chrisjfortier.com>
2020-06-05 10:31:33 -0700

    fix landing page and remove the hack that messed up scrolling on other pages

--- src/components/ui/AppTemplate.js
+++ src/components/ui/AppTemplate.js
@@ -4,7 +4,7 @@ import Navigation from "./Navigation";
 
 export default function AppTemplate(props) {
    return (
-      <div className="container app-contents">
+      <div className="container">
          <div className="row">
             <div className="col-12 col-xl-6 offset-xl-3 col-lg-8 offset-lg-2 col-md-10 offset-md-1">
                <Header />

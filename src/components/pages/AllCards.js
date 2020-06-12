e0b443dbcb79f75de143b058f89101d57f262374
Chris-Fortier <vendor@chrisjfortier.com>
2020-06-12 14:21:39 -0700

    apply "The Business Logic of Sorting Cards in White Bear" video

--- src/components/pages/AllCards.js
+++ src/components/pages/AllCards.js
@@ -4,60 +4,78 @@ import MemoryCard from "../ui/MemoryCard";
 import memoryCards from "../../mock-data/memory-cards";
 // const memoryCard = memoryCards[2];
 
-export default function AllCards() {
-   return (
-      <AppTemplate>
-         {/* <!-- form stuff --> */}
-         <form className="row mb-0">
-            <div className="form-group col-8">
-               <input
-                  className="form-control"
-                  id="search-for"
-                  placeholder="Search for a word"
-               />
-            </div>
-            <div className="col-4">
-               <button
-                  className="btn btn-primary btn-block btn-sm"
-                  id="search-button"
-                  type="button"
-               >
-                  Search
-               </button>
-            </div>
-         </form>
+export default class AllCards extends React.Component {
+   constructor(props) {
+      super(props);
+      this.state = {};
 
-         {/* <!-- select version --> */}
-         <form className="row mb-0">
-            <label htmlFor="cars" className="col-4">
-               Sort cards by
-            </label>
+      // EASY
+      // orderBy([totalSuccessfulAttempts, createdAt], [desc, desc])
 
-            <div className="form-group col-8">
-               <div className="dropdown">
-                  <select
-                     id="cars"
-                     className="float-right btn dropdown-toggle btn-block"
-                     style={{ height: "36px" }}
-                  >
-                     <option value="volvo">Most recent</option>
-                     <option value="saab">Oldest</option>
-                     {/* <option value="mercedes">Hardest</option>
-                     <option value="audi">Easiest</option> */}
-                  </select>
+      // HARD
+      // orderBy([totalSuccessfulAttempts, createdAt], [asc, asc])
+
+      // MOST RECENT
+      // orderBy([createdAt], [desc])
+
+      // OLDEST
+      // orderBy([createdAt], [asc])
+   }
+   render() {
+      return (
+         <AppTemplate>
+            {/* <!-- form stuff --> */}
+            <form className="row mb-0">
+               <div className="form-group col-8">
+                  <input
+                     className="form-control"
+                     id="search-for"
+                     placeholder="Search for a word"
+                  />
                </div>
-            </div>
-         </form>
+               <div className="col-4">
+                  <button
+                     className="btn btn-primary btn-block btn-sm"
+                     id="search-button"
+                     type="button"
+                  >
+                     Search
+                  </button>
+               </div>
+            </form>
 
-         {memoryCards.map((memoryCard) => {
-            return (
-               <MemoryCard
-                  answer={memoryCard.answer}
-                  imagery={memoryCard.imagery}
-                  key={memoryCard.id}
-               />
-            );
-         })}
-      </AppTemplate>
-   );
+            {/* <!-- select version --> */}
+            <form className="row mb-0">
+               <label htmlFor="cars" className="col-4">
+                  Sort cards by
+               </label>
+
+               <div className="form-group col-8">
+                  <div className="dropdown">
+                     <select
+                        id="cars"
+                        className="float-right btn dropdown-toggle btn-block"
+                        style={{ height: "36px" }}
+                     >
+                        <option value="volvo">Most recent</option>
+                        <option value="saab">Oldest</option>
+                        <option value="mercedes">Hardest</option>
+                        <option value="audi">Easiest</option>
+                     </select>
+                  </div>
+               </div>
+            </form>
+
+            {memoryCards.map((memoryCard) => {
+               return (
+                  <MemoryCard
+                     answer={memoryCard.answer}
+                     imagery={memoryCard.imagery}
+                     key={memoryCard.id}
+                  />
+               );
+            })}
+         </AppTemplate>
+      );
+   }
 }

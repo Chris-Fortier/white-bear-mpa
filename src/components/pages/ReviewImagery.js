e2c59c97b048a24071cd7a98800998feb2d1a423
Chris-Fortier <vendor@chrisjfortier.com>
2020-06-16 09:37:43 -0700

    apply "Getting Mock Data from an API Request with Axios" video

--- src/components/pages/ReviewImagery.js
+++ src/components/pages/ReviewImagery.js
@@ -2,37 +2,55 @@ import React from "react";
 import AppTemplate from "../ui/AppTemplate";
 import { Link } from "react-router-dom"; // a React element for linking
 import memoryCards from "../../mock-data/memory-cards";
+import axios from "axios";
+// import { render } from "node-sass";
 
 const memoryCard = memoryCards[2];
 
-export default function ReviewImagery() {
-   return (
-      <AppTemplate>
-         <div className="mb-5">
-            <div className="card bg-primary">
-               <div className="card-body">{memoryCard.imagery}</div>
-            </div>
-         </div>
-
-         {/* <!-- button row --> */}
-
-         <div className="row mb-4">
-            <div className="col">
-               <Link to="/review-answer" className="btn btn-link">
-                  Previous card
-               </Link>
-               <div className="float-right">
-                  <Link
-                     to="/review-answer"
-                     className="btn btn-outline-primary btn-lg"
-                  >
-                     Show answer
-                  </Link>
+export default class ReviewImagery extends React.Component {
+   constructor(props) {
+      super(props);
+      axios
+         .get("https://run.mocky.io/v3/830919fe-14ee-4918-ba47-fb8d7d0243d3")
+         .then(function (response) {
+            // handle success
+            console.log(response);
+         })
+         .catch(function (error) {
+            // handle error
+            console.log(error);
+         });
+      // delete url https://designer.mocky.io/manage/delete/830919fe-14ee-4918-ba47-fb8d7d0243d3/Sg97iH72WMbqEXbJbyUkBezCO20fYpm85qWY
+   }
+   render() {
+      return (
+         <AppTemplate>
+            <div className="mb-5">
+               <div className="card bg-primary">
+                  <div className="card-body">{memoryCard.imagery}</div>
                </div>
             </div>
-         </div>
 
-         {/* <!-- end button row -->          */}
-      </AppTemplate>
-   );
+            {/* <!-- button row --> */}
+
+            <div className="row mb-4">
+               <div className="col">
+                  <Link to="/review-answer" className="btn btn-link">
+                     Previous card
+                  </Link>
+                  <div className="float-right">
+                     <Link
+                        to="/review-answer"
+                        className="btn btn-outline-primary btn-lg"
+                     >
+                        Show answer
+                     </Link>
+                  </div>
+               </div>
+            </div>
+
+            {/* <!-- end button row -->          */}
+         </AppTemplate>
+      );
+   }
 }
